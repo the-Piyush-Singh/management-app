70581e19a9ec0db61791374edce5fe27ddc00384
the-Piyush-Singh <2200521520036@ietlucknow.ac.in>
2025-08-06 13:28:05 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,36 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+
 import Layout from './components/Layout'
 import HomePage from './pages/HomePage'
 import TaskListPage from './pages/TaskListPage'
 import ShowTask from './pages/ShowTask'
+
+// Define routes with future flags enabled
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        { index: true, element: <HomePage /> },
+        { path: 'task-list', element: <TaskListPage /> },
+        { path: 'show-task/:taskid', element: <ShowTask /> },
+      ],
+    },
+  ],
+  {
+    future: {
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+    },
+  }
+)
+
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes >
-        <Route path='/' element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path='/task-list' element={<TaskListPage />} />
-          <Route path='/show-task/:taskid' element={<ShowTask />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
